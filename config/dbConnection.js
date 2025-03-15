03880077f771d3002a118cfcd74513d6dd8bffe8
tausif892 <63648692+tausif892@users.noreply.github.com>
2025-03-15 17:53:52 +0530

    Mongoose string hardcoded

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async() => {
     try{
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect("mongodb+srv://tausif:1Pathanwadi@bloodtracker.03l2z.mongodb.net/bloodtracker?retryWrites=true&w=majority&appName=bloodtracker");
         console.log(`Database connected`, connect.connection.host, connect.connection.name);
     }catch(err){
         console.log(err);
